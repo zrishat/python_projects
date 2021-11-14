8d7f7a576ae5a9cf292c12434cfe01ed315df680
Rishat Zainullin <r.zainullin@omp.ru>
2021-11-14 17:05:15 +0300

    Added tests for class and functions

--- lotto_game/test/test_game.py
+++ lotto_game/test/test_game.py
@@ -1,25 +1,43 @@
-from start_game import all_numbers_is_out
+import pytest
 
+from lotto_game.Bag import Bag
+from lotto_game.Player import Player
+from lotto_game.utils import all_numbers_is_out, show_formatted_card
+from lotto_game.configurations import CPU_TYPE
 
 class TestGame:
     def test_player_init(self):
-        pass
+        player = Player()
+        assert player, "Не инициализируется класс Player"
 
     def test_player_generate_card(self):
-        pass
+        player = Player()
+        assert player.player_card
 
     def test_bag_init(self):
         bag = Bag()
-        assert bag
+        assert bag,"Не инициализируется класс Bag"
 
     def test_bag_str(self):
-        pass
+        bag = Bag()
+        val = bag.__str__()
+        assert 'Новый бочонок' in val and 'осталось' in val
 
-    def test_show_formatted_card(self):
-        pass
+    def test_show_formatted_card(self, capsys):
+        player = Player()
+        show_formatted_card(player.player_card)
+        captured = capsys.readouterr()
+        assert "-"*30 == captured.out[0:30], \
+            "Неверное форматирование первой строки"
 
-    def test_all_numbers_is_out(self):
-        assert all_numbers_is_out(['', '--'])
+    @pytest.mark.parametrize("player_card", [
+        ['', '--'],
+        ['--', ''],
+        [1, 2]
+    ])
+    def test_all_numbers_is_out(self, player_card):
+        assert all_numbers_is_out(player_card), \
+            "Возвращается неверное значение"
 
-    def test_all_game_process(self):
-        pass
+    def test_cpu_defined(self):
+        assert CPU_TYPE, "Не установлена переменная CPU_TYPE"
